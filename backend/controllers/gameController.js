706b661fb21235dc3690df5ed087d6987eec8594
Alex M <alexandre.munier@efrei.net>
2025-05-12 19:47:50 +0200

    ça passe

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -205,7 +205,7 @@ module.exports.getGames = async (req, res) => {
   
       // Requête avec jointure entre feedback et users (supposons que la table des utilisateurs s'appelle `users`)
       const [feedbacks] = await db.execute(
-        `SELECT * FROM view_feedback_user WHERE game_ID = ?`,
+        `SELECT * FROM feedback WHERE game_ID = ?`,
         [gameId]
       );
 
