103c4bb7f46e92a5aa52fd9815b087fbf3d6c9c8
Alex M <alexandre.munier@efrei.net>
2025-05-12 20:05:55 +0200

    logssf

--- backend/routes/games.js
+++ backend/routes/games.js
@@ -7,7 +7,7 @@ let cache = apicache.middleware;
 router.get("/games", cache('5 minutes'), games.getGames);
 router.get("/allgames", cache('5 minutes'), games.getAllGames);
 router.get('/categories', cache('5 minutes'), games.getAllCategories);
-router.get('/getfeedback/:id', games.getFeedbackByGameId);
+router.get('/getfeedback/:gameId', games.getFeedbackByGameId);
 router.post('/addfeedback', games.addFeedback);
 router.get('/game/:slug', games.getGameBySlug);
 
